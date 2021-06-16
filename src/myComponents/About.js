cfded3f3232073353de08bd1b4a0ae734f482823
Ashu Raj <71003775+ashu-raj-1999@users.noreply.github.com>
2021-06-16 22:58:54 +0530

    todos items

---
+++ src/myComponents/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+           Thankyou For using ToDOs List App
+            <p>It's owner is Ashu Raj Shandilya. 
+                This is first project created using React JS .Hope you find it Good.Many more features will be added further. 
+                Once again Thankq for using. 
+            </p>
+            <p>
+                Contact:- ashuraj1226@gmail.com
+            </p>
+        </div>
+    )
+}
+
---
+++ src/myComponents/AddTodo.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+export const AddTodo = ({ addTodo }) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+
+    const submit = (e) => {
+        e.preventDefault();
+        if (!title || !desc) {
+            alert("Title of Description must not be empty");
+        }
+        else {
+            addTodo(title, desc);
+            setTitle("");
+            setDesc("");
+        }
+    }
+    return (
+        <div className="Container my-3">
+            <h2>Add a ToDo</h2>
+            <form onSubmit={submit}>
+                <div className="mb-3">
+                    <label htmlFor="title" className="form-label">Todo Title</label>
+                    <input type="text" value={title} onChange={(e) => { setTitle(e.target.value) }} className="form-control" id="title" aria-describedby="emailHelp" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label">Todo Description</label>
+                    <input type="text" value={desc} onChange={(e) => { setDesc(e.target.value) }} className="form-control" id="desc" />
+                </div>
+
+                <button type="submit" className="btn btn-sn btn-success">Add Todo</button>
+            </form>
+        </div>
+    )
+}
